943229a695e8b0e4c2602a0184dc4568cec1b203
hoangduc170803 <hoangduc170803@gmail.com>
2026-01-19 20:13:39 +0700

    first commit

---
+++ src/main/java/com/example/EXE201c/Exe201cApplication.java
@@ -0,0 +1,13 @@
+package com.example.EXE201c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exe201cApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exe201cApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EXE201c/Exe201cApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EXE201c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exe201cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
